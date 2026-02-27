9ad21ff95550ee6da1fb6354d364c2127163b714
DiarraMaim01 <maimouna_diarra@reseau.eseo.fr>
2026-02-27 15:52:32 +0100

    feat: implement core components and routing system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Tests } from './tests/tests';
+import { Admin } from './admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },              // Page d'accueil
+  { path: 'login', component: Login },        // Page login
+  { path: 'register', component: Register },  // Page inscription
+  { path: 'tests', component: Tests },        // Page des tests
+  { path: 'admin', component: Admin },        // Page admin
+  { path: '**', redirectTo: '' }                       // Redirection si route inconnue
+];
