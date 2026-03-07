d27bf450e6e72416db87f226a30a64edcae06c25
DiarraMaim01 <maimouna_diarra@reseau.eseo.fr>
2026-03-07 18:56:10 +0100

    fix: use environment instead of environment.prod

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -3,7 +3,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { LoginRequest, RegisterRequest, AuthResponse } from '../models/auth';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
